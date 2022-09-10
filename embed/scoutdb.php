ce4b40ca3c86bc44f80d3916a0bc78f14a30b623
ovarz <ovarz_03@yahoo.com>
2022-09-10 20:47:22 +0700

    ADDS

--- embed/scoutdb.php
+++ embed/scoutdb.php
@@ -1,11 +1,11 @@
 <style>
   *, html{margin:0; padding:0; box-sizing:border-box;}
   .video-container{position:relative;}
-  .video-container:before{content:''; display:block; padding-top:157%;}
+  .video-container:before{content:''; display:block; padding-top:222.22%;}
   .video-container video{position:absolute; top:0; left:0; width:100%; height:100%;}
 </style>
 <div class="video-container">
   <video width="320" height="240" autoplay loop muted>
-    <source src="strimid.mp4" type="video/mp4">
+    <source src="scoutdb.mp4" type="video/mp4">
   </video>
 </div>
